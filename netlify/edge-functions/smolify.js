171ec4986ba31fcd8d4effdee1f3798d18b488d9
Michael Jolley <mike@baldbeardedbuilder.com>
2024-01-30 11:07:39 -0600

    Fixing function route

--- netlify/edge-functions/smolify.js
+++ netlify/edge-functions/smolify.js
@@ -1,4 +1,31 @@
-import { getShortUrl } from './scripts/supabase';
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
+
+const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_ANON_KEY,
+  {
+    auth: {
+      persistSession: false,
+    },
+  }
+);
+
+async function getShortUrl(slug) {
+  const { data, error } = await supabase
+    .from('shorturls')
+    .select()
+    .eq('slug', slug);
+
+  if (data && data[0] && data[0].target) {
+    return data[0].target;
+  }
+
+  if (error) {
+    console.error(error);
+  }
+
+  return undefined;
+}
 
 export default async function handler(req) {
   // Search for the placeholder
